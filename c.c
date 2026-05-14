1251a7d4726c072445f1eabb5eed37366ef4a16a
yisea <haiyangyi873@gmail.com>
2026-05-14 03:45:32 -0400

    c.c

--- c.c
+++ c.c
@@ -1,7 +1,20 @@
-#include<stdio.h>
- int main()
+klnclude<stdio.h>
+int main()
 {
     printf ("step 1");
     return 0;
 
 }
+    printf ("step 1");
+{
+{
+    printf ("step 1");
+    printf ("step 1");
+    printf ("step 1");
+    printf ("step 1");
+    printf ("step 1");
+    printf ("step 1");
+    printf ("step 1");
+    printf ("step 1");
+    printf ("step 1");
+    printf ("step 1");
